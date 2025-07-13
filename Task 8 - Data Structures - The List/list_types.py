45837c1b03730a60da1c419b4f12fa6d8497eec5
Benjamin <benwj2005@gmail.com>
2025-07-13 20:14:18 +0200

    Create list_types.py

---
+++ Task 8 - Data Structures - The List/list_types.py
@@ -0,0 +1,8 @@
+friend_list=[input(),input(),input()]
+length= len(friend_list)
+print(friend_list[0] + " " + friend_list[2])
+print (length)
+friends_age= [input(),input(),input()]
+print (friend_list[0] + "is",friends_age[0],"years old")
+print (friend_list[1] + "is", friends_age[1],"years old")
+print (friend_list[2] + "is", friends_age[2],"years old")
