fa0157519fb385360d6f4a855f9dc583e6205b49
Benjamin <benwj2005@gmail.com>
2025-07-16 00:18:55 +0200

    meow

---
+++ Task 9 - Iteration/pattern.py
@@ -0,0 +1,5 @@
+for x in range(0, 8):
+    if x >= 6:
+        print("*" * (9 - x))
+    else:
+        print("*" * x)
--- Task 9 - Iteration/while.py
+++ Task 9 - Iteration/while.py
@@ -1,4 +1,13 @@
+total = 0
+numbers = 0
+average = 0
 while True:
-    number=int(input())
+    number = int(input())
     if number == -1:
-        print ("End Of The Line")
+         break
+    if number == 0:
+         continue
+    total += number
+    numbers += 1
+average = total / numbers
+print(average)
