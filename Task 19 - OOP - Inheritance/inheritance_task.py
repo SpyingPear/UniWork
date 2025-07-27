1c54e89cfc58666ca260121e5a8950c6944ac742
Benjamin <benwj2005@gmail.com>
2025-07-28 00:16:53 +0200

    z

--- Task 19 - OOP - Inheritance/inheritance_task.py
+++ Task 19 - OOP - Inheritance/inheritance_task.py
@@ -1,18 +1,26 @@
 class Course:
-    # Class attribute for the course name
     name = "Fundamentals of Computer Science"
 
-    # Class attribute for the contact website
     contact_website = "www.hyperiondev.com"
 
-    # Method to display contact details
     def contact_details(self):
         print("Please contact us by visiting", self.contact_website)
 
+    def head_office(self):
+        print("Head office: Cape Town")
 
-# Example usage:
-# Create an instance of the Course class
-course = Course()
+class OOPCourse(Course):
+    def __init__(self):
+        self.description = "OOP Fundamentals"
+        self.trainer = "Mr Anon A. Mouse"
 
-# Call the contact_details method to display contact information
-course.contact_details()
+    def trainer_details(self):
+        print(f"This course is about {self.description}. The trainer is {self.trainer}.")
+
+    def show_course_id(self):
+        print("Course ID: #12345")
+
+course_1 = OOPCourse()
+course_1.contact_details()
+course_1.trainer_details()
+course_1.show_course_id()
